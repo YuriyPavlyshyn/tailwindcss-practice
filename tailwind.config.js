f99dd62a9bef26379e0936dc917acdbcca3f9cdb
Yuriy Pavlyshyn <yuriypavlyshyn3@gmail.com>
2023-08-10 23:43:43 +0300

    Applied a new font and added a custom style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   content: ["./index.html"],
   /* darkMode: 'class', */  /* для додавання темної теми на сторінку */
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'logo-main': '#73117c',
+      },
+      fontFamily: {
+        main: ['Bruno Ace'],
+      },
+    },
   },
   plugins: [],
 }
